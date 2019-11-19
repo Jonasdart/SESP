8436b9a6f4056f92f96c37c35c1be750f0655531
duzzsys <duzzsystem@gmail.com>
2019-11-18 21:29:30 -0300

    Foi alterado o estilo de visualização

--- view_controller.py
+++ view_controller.py
@@ -23,7 +23,7 @@ class sesp_view():
         except:
             pass
 
-        self.altura = int(self.tela.winfo_screenheight()- 75)
+        self.altura = int(self.tela.winfo_screenheight()+100)
         self.largura = int(self.tela.winfo_screenwidth())
 
         if self.largura/2/4 > 100:
@@ -33,45 +33,88 @@ class sesp_view():
 
         self.tela.state("zoomed")
 
-    def botoes_menu(self):
+    def mostra_esconde_botoes(self, mostrar = True):
+        if mostrar:
+            self.widgets_botoes_menu = self.botoes_menu()
+            for i in range(len(self.widgets_botoes_menu)):
+                self.widgets_botoes_menu[i].place(x=f"{self.botoes_menu_x[0]}", y = f"{self.botoes_menu_y[i]}")
+            self.botao_lateral["text"] = "<"
+            self.botao_lateral["command"] = lambda: self.mostra_esconde_botoes(mostrar = False)
+        else:
+            for i in range(len(self.widgets_botoes_menu)):
+                self.widgets_botoes_menu[i].place_forget()
+                self.botao_lateral["text"] = ">"
+                self.botao_lateral["command"] = lambda: self.mostra_esconde_botoes()
+
+    def botoes_menu(self, tela = None):
+        self.posiciona_botoes_menu()
+
+        if tela is None:
+            tela = self.tela
+
+        verificar_internet = Button(tela, text = "Internet Não Funciona", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
+            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "2", width = "20", 
+            bd = "1", relief = "flat", overrelief = "sunken")
+        verificar_spdata = Button(tela, text = "SPDATA Não Abre", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
+            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "2", width = "20", 
+            bd = "1", relief = "flat", overrelief = "sunken")
+        verificar_travamento_spdata = Button(tela, text = "SPDATA Travando", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
+            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "2", width = "20", 
+            bd = "1", relief = "flat", overrelief = "sunken")
+        verificar_glpi = Button(tela, text = "GLPI Sem Acesso", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
+            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "2", width = "20", 
+            bd = "1", relief = "flat", overrelief = "sunken")
+        verificar_computador = Button(tela, text = "Computador Travando", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
+            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "2", width = "20", 
+            bd = "1", relief = "flat", overrelief = "sunken")
+        verificar_impressora = Button(tela, text = "Não Consigo Imprimir", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
+            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "2", width = "20", 
+            bd = "1", relief = "flat", overrelief = "sunken")
+
+        return [verificar_internet, verificar_spdata, verificar_travamento_spdata, verificar_glpi, verificar_computador, verificar_impressora]
+
+
+    def botoes_controle(self, tela = None):
+        self.posiciona_botoes_controle()
+
+        if tela is None:
+            tela = self.tela
+
+        meu_computador = Button(tela, text = "Meu Computador", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
+            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "2", width = "20", 
+            bd = "1", relief = "flat", overrelief = "sunken", command = self.backend.busca_cabecalho)
+        self.botao_lateral = Button(tela, justify = "left", text = ">", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}", "bold"), fg = "white", 
+            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "40", width = "2", 
+            bd = "1", relief = "flat", overrelief = "sunken", command = lambda: self.mostra_esconde_botoes())
+
+        meu_computador.place(x=f"{self.botoes_controle_x[1]}", y = f"{self.botoes_controle_y[1]}")
+        self.botao_lateral.place(x=f"{self.botoes_controle_x[0]}", y = f"{self.botoes_controle_y[0]}")
+
+    def posiciona_botoes_menu(self):
+        #eixo x
         self.botoes_menu_x.append((self.largura/2)/10)
-        self.botoes_menu_y.append((self.altura/2)/4)
-        self.botoes_menu_y.append((self.altura/2)/1.27)
-        self.botoes_menu_y.append(self.altura/1.5)
+        #eixo y
+        self.botoes_menu_y.append((self.altura/1)/8)
+        self.botoes_menu_y.append((self.altura/1)/5)
+        self.botoes_menu_y.append((self.altura/1)/3.62)
+        self.botoes_menu_y.append((self.altura/1)/2.85)
+        self.botoes_menu_y.append((self.altura/1)/2.35)
+        self.botoes_menu_y.append((self.altura/1)/2)
 
 
-    def botoes_controle(self):
-        self.botoes_controle_x.append((self.largura/1.18))
-        self.botoes_controle_y.append((self.altura/2)/4)
+    def posiciona_botoes_controle(self):
+        self.botoes_controle_x.append(1)
+        self.botoes_controle_x.append(self.largura/1.18)
+        self.botoes_controle_y.append(1)
+        self.botoes_controle_y.append((self.altura/2)/20)
 
     def tela_inicial(self):
         self.tela = Tk()
         self.posiciona_janela()
-        self.botoes_menu()
         self.botoes_controle()
         self.tela.geometry(f"{self.largura}x{self.altura}+0+0")
         self.tela["bg"] = "#193E4D"
-
-        verificar_internet = Button(self.tela, text = "Verificar Internet", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
-            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "8", width = "17", 
-            bd = "1", relief = "flat", overrelief = "sunken")
-        verificar_spdata = Button(self.tela, text = "Verificar SPDATA", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
-            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "8", width = "17", 
-            bd = "1", relief = "flat", overrelief = "sunken")
-        verificar_impressora = Button(self.tela, text = "Verificar Impressora", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
-            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "8", width = "17", 
-            bd = "1", relief = "flat", overrelief = "sunken")
-
-        meu_computador = Button(self.tela, text = "Meu Computador", bg = "#0B1F22", font = ("Verdana", f"{self.tamanho_fonte_botoes}"), fg = "white", 
-            highlightcolor = "white", activebackground = "#193E4D", activeforeground = "white", height = "2", width = "20", 
-            bd = "1", relief = "flat", overrelief = "sunken", command = self.backend.busca_cabecalho)
-
-
-
-        verificar_internet.place(x=f"{self.botoes_menu_x[0]}", y = f"{self.botoes_menu_y[0]}")
-        verificar_spdata.place(x=f"{self.botoes_menu_x[0]}", y = f"{self.botoes_menu_y[1]}")
-        verificar_impressora.place(x=f"{self.botoes_menu_x[0]}", y = f"{self.botoes_menu_y[2]}")
-        meu_computador.place(x=f"{self.botoes_controle_x[0]}", y = f"{self.botoes_controle_y[0]}")    
+ 
         self.tela.mainloop()
 
 
