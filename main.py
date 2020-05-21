61c839dc3211c180698110005e82d20f705e6cce
Jonas Duarte <duzzsystem@gmail.com>
2020-05-21 17:58:48 -0300

    Adicionando requests para a api

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import configparser
 import subprocess
+import requests
+import json
 from platform import node
 from os import system
 
 
 class Backend():
     def __init__(self):
+        self.base_url = self.get_api_server()
+
+    def teste(self):
         try:
             computer_info = self.get_computer()
 
-            print(computer_info)
+            self.base_url = self.get_api_server()
 
             inventory_number = computer_info['InventoryNumber']
             self.rename_computer(inventory_number)
         except Exception as e:
             print(e)
 
+
+    def get_api_server(self):
+        config = configparser.ConfigParser()
+        config.read('conf.cfg')
+
+        base_url = config.get('config_api', 'base_url')
+
+        return base_url
 
     def get_computer(self):
         config = configparser.ConfigParser()
@@ -38,6 +51,17 @@ class Backend():
         }
 
 
+    def get_computer_from_server(self, inventory_number):
+        url = self.base_url+'/get_computer'
+
+        data = json.dumps({
+            "InventoryNumber" : inventory_number,
+            "Database" : "GLPI"
+        })
+
+        requests.post(url=url, json=data)
+
+
     def force_four_digits(self, inventory_number):
         number_of_zeros = 4 - len(inventory_number)
         inventory_number = '0'*number_of_zeros + inventory_number
@@ -51,10 +75,11 @@ class Backend():
 
         system(f'wmic computersystem where name="{old_name}" rename "{new_name}"')
 
-        reiniciar = input('Reiniciar agora?(S/n): ')
+        reiniciar = input('Reiniciar agora?(S/n): ').upper()
         if reiniciar == 'S':
             subprocess.run(['shutdown', '-r', '-t', '1'])
 
-
+'''
 if __name__ == "__main__":
     Backend()
+'''
