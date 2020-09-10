6c77207cda049a9ca8ee5dcec594594c4d59f04c
Jonas Duarte <duzzsystem@gmail.com>
2020-09-10 10:30:21 -0300

    Versão atualizada para 0.0.5-DEV

--- output/inst.bat
+++
@@ -1,6 +0,0 @@
-cd C:\
-git clone https://github.com/duzzsys/SESP.git
-cd SESP
-git clone -b master_version https://github.com/duzzsys/SESP.git Atualizações       
-start main.py 0408 install
-        
